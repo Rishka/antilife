9e1c4f1cc9743851a043c8f78079e819b9481de3
Rishka <aperlman@me.com>
2015-04-30 19:37:40 -0400

    Init for antilife

---
+++ Assets/Character/Animations.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+    
+  public Animator Animator {get; set;}
+  public float V {get; set;}
+  public float H {get; set;}
+  public float Sprint {get; set;}
+  public bool Aiming  {get; set;}
+    // Use this for initialization
+    void Start () {
+	Animator = GetComponent<Animator>();
+        
+    }
+	
+    // Update is called once per frame
+    void Update () {
+	
+    }
+    
+    void FixedUpdate()
+    {
+        Sprinting();
+        V = Input.GetAxis("Vertical");
+        H = Input.GetAxis("Horizontal");
+        Aiming = gameObject.GetComponent<PlayerController>().shooting;
+        Animator.SetFloat ("Walk", V);
+        Animator.SetFloat ("Turn", H);
+        Animator.SetFloat("Sprint", Sprint);
+        Animator.SetBool("Aiming", Aiming);
+        
+    }
+    void Sprinting () {
+        if(Input.GetButton("Jump")) {
+            Sprint = 1F;
+        }
+        else 
+        {    
+            Sprint = 0.0F;
+        }
+    }
+}
+
---
+++ Assets/Triggerscript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triggerscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void print1()
+	{
+		print ("Start trigger1");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,1.0F,0);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(this.gameObject);
+		print1();
+
+		//DialogScript.DialogueStart();
+
+	}
+
+}
