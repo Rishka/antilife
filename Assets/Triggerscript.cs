643b161771626f15c58ad628d42ec8772359b587
Rishka <aperlman@me.com>
2015-05-01 00:19:22 -0400

    added main menu

--- Assets/Triggerscript.cs
+++ Assets/Triggerscript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Triggerscript : MonoBehaviour {
   bool active;
+  public static int i;
+  public static GameObject[] triggers = new GameObject[13];
   // Use this for initialization
   void Start () {
     active = true;
@@ -11,19 +13,23 @@ public class Triggerscript : MonoBehaviour {
   void print1()
   {
     active = false;
-    StartCoroutine(DialogScript.DialogueStart("1", gameObject));
+    i++;
+    StartCoroutine(DialogScript.DialogueStart(i.ToString(), gameObject));
+
   }
 
   // Update is called once per frame
   void Update () {
-
+    var rot = Quaternion.identity;
+    GameMenu.RotateCompass(rot);
     transform.Rotate(0,1.0F,0);
+    
   }
 
   void OnTriggerEnter(Collider other) {
-    if(active)
+    if(gameObject.Equals(triggers[i]))
       print1();
-
+    
     //DialogScript.DialogueStart();
 
   }
